0cbd7cbb8023f1b93bc2d31622a9a94e0ab47c7e
flyingliu <fly0733@gmail.com>
2015-05-22 16:07:29 +0800

    user

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var mongoStore = require('connect-mongo')(session);
+
 var multer = require('multer');
 var mongoose = require('mongoose');
 var Movie = require('./models/movie');
+var User = require('./models/user');
 var _ = require('underscore');
 var serveStatic = require('serve-static');
 var port = process.env.PORT || 3000;
 var app = express();
+var dbUrl = 'mongodb://imooc:fly2431757@ds041188.mongolab.com:41188/imooc';
+if(process.env.NODE_ENV === "development") {
+  mongoose.connect('mongodb://localhost/imooc');
+} else {
+  mongoose.connect(dbUrl);
+}
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(multer()); // for parsing multipart/form-data
-app.locals.moment = require('moment')
+app.locals.moment = require('moment');
+app.use(cookieParser());
+app.use(session({
+  secret: 'imooc',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false },
+  store: new mongoStore({
+    url: dbUrl,
+    collection: 'sessions'
+  })
+}))
 
-if(process.env === "development") {
-  mongoose.connect('mongodb://localhost/imooc');
-} else {
-  mongoose.connect('mongodb://imooc:fly2431757@ds041188.mongolab.com:41188/imooc');
-}
 
 
 app.set('views','./views/pages')
@@ -31,6 +48,7 @@ app.listen(port);
 console.log('imooc started on port' + port);
 
 app.get('/',function(req,res){
+  console.log(req.session.user);
   Movie.fetch(function(err,movies) {
     if (err) {
       console.log(err);
@@ -41,6 +59,61 @@ app.get('/',function(req,res){
     });
   })
 })
+
+// user signup
+app.post('/user/signup', function(req,res) {
+  var _user = req.body.user;
+  User.find({name: _user.name}, function(err, user) {
+    if (err) {
+      console.log(err);
+    }
+    if(user.length) {
+      return res.redirect('/')
+    } else {
+      var user = new User(_user);
+      user.save(function(err, user){
+        if(err) {
+          console.log(err);
+        }
+        res.redirect('/admin/userlist')
+      });
+    }
+  })
+
+})
+
+
+// user signin
+app.post('/user/signin', function(req, res) {
+  var _user = req.body.user;
+  var name = _user.name;
+  var password = _user.password;
+  User.findOne({name: name,password: password}, function(err, user) {
+    if (err) console.log(err);
+    if (!user) return res.redirect('/admin/list');
+    req.session.user = user;
+    return res.redirect('/');
+  });
+})
+
+//user list
+
+app.get('/admin/userlist',function(req,res){
+  User.fetch(function(err,Users) {
+    if (err) {
+      console.log(err);
+    }
+    res.render('userlist',{
+      title:'imooc userlist',
+      users:Users
+    });
+  })
+})
+
+
+
+
+
 
 app.get('/movie/:id',function(req,res){
   var id = req.params.id;
@@ -154,4 +227,6 @@ app.get('/admin/list',function(req,res){
     });
   })
 })
+
+
 
